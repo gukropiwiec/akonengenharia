3e20a0176d1e9062ace80306203c10eb58a20ea4
André Luiz Bartholdy <andreluizbart@gmail.com>
2023-07-25 23:04:40 -0300

    creation section home and about

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,8 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionHomeComponent } from './section-home/section-home.component';
+import { SectionAboutComponent } from './section-about/section-about.component';
 
-const moduleComponents = [HeaderComponent];
+const moduleComponents = [HeaderComponent, FooterComponent, SectionHomeComponent, SectionAboutComponent];
 
 @NgModule({
     imports: [
---
+++ src/app/components/section-home/section-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-section-home',
+    templateUrl: './section-home.component.html',
+    styleUrls: ['./section-home.component.scss']
+})
+export class SectionHomeComponent {
+
+}
